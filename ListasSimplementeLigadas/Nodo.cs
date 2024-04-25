69491c130ca3fac9827211c1e2f76006b2c18791
Edith Ochoa Ramos <edith_112002@hotmail.com>
2024-04-24 22:59:27 -0700

    Add project files.

---
+++ ListasSimplementeLigadas/Nodo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ListasSimplementeLigadas
+{
+    public class Nodo
+    {
+        public string Value { get; set; }
+        public Nodo Link { get; set; }
+        public Nodo(string value = "", Nodo link = null)
+        {
+            Value = value;
+            Link = link;
+        }
+
+    }
+}
---
+++ ListasSimplementeLigadas/Program.cs
@@ -0,0 +1,10 @@
+﻿using ListasSimplementeLigadas;
+
+Console.WriteLine("Listas Simplemente Ligadas");
+
+Nodo nodoEmpty = new Nodo();
+Nodo nodo = new Nodo("Pepe", nodoEmpty);
+Console.WriteLine(nodoEmpty.Value);
+Console.WriteLine(nodoEmpty.Value);
+
+//Console.WriteLine(nodo.Name);
